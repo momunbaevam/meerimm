5a816d23bff984ad05be0795be44344169822dc8
momunbaevam <93367394+momunbaevam@users.noreply.github.com>
2021-11-12 13:14:10 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
-<html>
-<body>
-
-<h2>HTML Image</h2>
-<img src="pic_trulli.jpg" alt="Trulli" width="500" height="333">
-
-</body>
-</html>
+ <picture>
+  <source media="(min-width: 650px)" srcset="img_food.jpg">
+  <source media="(min-width: 465px)" srcset="img_car.jpg">
+  <img src="img_girl.jpg">
+</picture> 
